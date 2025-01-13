fb573c938ca0ca1ffe2b4ab49f39e3839eaa8ee8
di1112 <106777047+di1112@users.noreply.github.com>
2025-01-13 01:23:50 -0500

    Add files via upload

---
+++ src/preprocessing.sql
@@ -0,0 +1,88 @@
+/* 
+Instagram-User-Analytics
+*/
+--Checking and Storing all Tables--
+
+--users table
+SELECT *
+FROM users;
+
+
+--photos table
+SELECT *
+FROM photos;
+
+--comments table
+SELECT *
+FROM comments;
+
+--likes table
+SELECT *
+FROM likes;
+
+--follows table
+SELECT *
+FROM follows;
+
+--tags table
+SELECT *
+FROM tags;
+
+--photo_tags table
+SELECT *
+FROM photo_tags;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
